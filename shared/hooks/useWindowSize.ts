8a5ed06972a09f6b425be34f6537432d8b685313
Anton <Yanastenokk@gmail.com>
2025-04-24 16:14:27 +0400

    fix: исправление хука useWindowSize

--- shared/hooks/useWindowSize.ts
+++ shared/hooks/useWindowSize.ts
@@ -1,12 +1,23 @@
 import { useEffect, useState } from "react"
 
-export default function useWindowSize():number {
-	const [width, setWidth] = useState(window.innerWidth)
-	const handleResize = () => setWidth(window.innerWidth)
-	useEffect(() => {
-		handleResize()
-		window.addEventListener('resize', handleResize)
-		return () => window.removeEventListener('resize', handleResize)
-	}, [])
-	return width
+export default function useWindowSize(): number {
+  const [windowWidth, setWindowWidth] = useState<number>(0)
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const handleResize = () => {
+        setWindowWidth(window.innerWidth)
+      }
+
+      handleResize()
+
+      window.addEventListener("resize", handleResize)
+
+      return () => {
+        window.removeEventListener("resize", handleResize)
+      }
+    }
+  }, []) 
+
+  return windowWidth
 }
