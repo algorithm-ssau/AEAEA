2d8e01582f326ef0cdebc0900725213c960950bc
Anton <Yanastenokk@gmail.com>
2025-04-24 18:36:35 +0400

    feat: доработка компонента AboutPopover

--- shared/components/ui/AboutPopover/AboutPopover.tsx
+++ shared/components/ui/AboutPopover/AboutPopover.tsx
@@ -1,8 +1,29 @@
-import { Popover } from "@mui/material";
+import { Box, Popover } from "@mui/material";
+import styles from "./AboutPopover.module.scss";
+import Link from "next/link";
 interface AboutPopoverProps {
     setAnchorEl: (anchorEl: HTMLButtonElement | null) => void;
     anchorEl: HTMLButtonElement | null;
 }
+
+const array = [
+    {
+        title: "Магазины",
+        href: "/shops",
+    },
+    {
+        title: "Реквизиты",
+        href: "/requisites",
+    },
+    {
+        title: "Лицензия",
+        href: "/license",
+    },
+    {
+        title: "Адреса",
+        href: "/addresses",
+    },
+];
 const AboutPopover: React.FC<AboutPopoverProps> = ({
     anchorEl,
     setAnchorEl,
@@ -20,20 +41,32 @@ const AboutPopover: React.FC<AboutPopoverProps> = ({
                 anchorEl={anchorEl}
                 onClose={handleClose}
                 anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'center',
-                  }}
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'center',
-                  }}
+                    vertical: "bottom",
+                    horizontal: "center",
+                }}
+                transformOrigin={{
+                    vertical: "top",
+                    horizontal: "center",
+                }}
             >
-                <ul>
-                    <li>ddddd</li>
-                    <li>ddddd</li>
-                    <li>ddddd</li>
-                    <li>ddddd</li>
-                </ul>
+                <Box sx={{ bgcolor: "#000", borderRadius: "10px" }}>
+                    <div className={styles.inner}>
+                        <nav>
+                            <ul className={styles.list}>
+                                {array.map((element) => (
+                                    <li className="" key={element.title}>
+                                        <Link
+                                            href={element.href}
+                                            className={styles.link}
+                                        >
+                                            {element.title}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </nav>
+                    </div>
+                </Box>
             </Popover>
         </>
     );
