118e0254fc71f3f2d56977405eaedb7c4ec4e5b0
Anton <cart0003@mail.ru>
2025-04-27 04:47:52 +0400

    refactor: декомпозиция ShopsPage

--- pages/ShopsPage/ShopsPage.tsx
+++ pages/ShopsPage/ShopsPage.tsx
@@ -1,71 +1,17 @@
 import { PlaceListItem } from "@/shared/components/ui/PlaceListItem";
 import styles from "./ShopsPage.module.scss";
+import {
+    placesNovokuybyshevskArray,
+    placesSamaraArray,
+} from "@/public/arrays/placesArray";
 
-import image1 from '../../public/images/place1.jpg'
-import image2 from '../../public/images/place2.jpg'
-import image3 from '../../public/images/place3.jpg'
-
-const array = [
-    {
-        title: 'ул.Вилоновская, 138"Е"',
-        time: "10.00 до 20.00, без выходных",
-        tel: "+7(846)922-93-87 (сот.)",
-        image: image1 ,
-    },
-    {
-        title: "пл. им Кирова, павильон 21",
-        time: "10.00 до 20.00, без выходных",
-        tel: "+7(937)202-04-51",
-        image: image2,
-    },
-    {
-        title: "Московское шоссе, д.205/1",
-        time: "12:00 до 20:00, Выходной: Понедельник",
-        tel: "+7(937)066-94-00",
-        image: image3,
-    },
-    {
-        title: "Московское шоссе 18-й км, д.25А",
-        time: "12:00 до 20:00 Выходной: Вторник",
-        tel: "+7(927)607-21-15",
-        image: image1,
-    },
-    {
-        title: 'Красноглинское шоссе, 3"Б"',
-        time: "10.00 до 20.00, без выходных",
-        tel: "+7(927)712-93-89",
-        image: image1,
-    },
-    {
-        title: "ул.Тамбовская, 2 (116 км)",
-        time: "Временно не работает",
-        tel: "+7(846)989-31-74 (сот.)",
-        image: image1,
-    },
-];
-
-const array2 = [
-    {
-        title: "проспект Победы, д.36",
-        time: "10.00 до 22.00, без выходных",
-        tel: "+7(937)062-70-11",
-        image: image1,
-    },
-    {
-        title: 'проспект Победы, д. 1"Ж"',
-        time: "10.00 до 22.00, без выходных",
-        tel: "+7(995)933-63-63",
-        image: image1,
-    },
-];
-
-export default function ShopsPage(){
+export default function ShopsPage() {
     return (
         <div className={styles.inner}>
             <h1 className={styles.title}>ГДЕ НАС НАЙТИ</h1>
             <h2 className={styles.subtitle}>г. Самара</h2>
             <ul className={styles.list}>
-                {array.map((element, index) => (
+                {placesSamaraArray.map((element, index) => (
                     <PlaceListItem
                         title={element.title}
                         index={index + 1}
@@ -79,7 +25,7 @@ export default function ShopsPage(){
             </ul>
             <h2 className={styles.subtitle}>г. Новокуйбышевск</h2>
             <ul className={styles.list}>
-                {array2.map((element, index) => (
+                {placesNovokuybyshevskArray.map((element, index) => (
                     <PlaceListItem
                         title={element.title}
                         index={index + 1}
@@ -99,4 +45,4 @@ export default function ShopsPage(){
             </p>
         </div>
     );
-};
+}
---
+++ public/arrays/placesArray.ts
@@ -0,0 +1,63 @@
+import image1 from '@/public/images/place1.jpg'
+import image2 from '@/public/images/place2.jpg'
+import image3 from '@/public/images/place3.jpg'
+import image4 from '@/public/images/place4.jpg'
+import image5 from '@/public/images/place5.jpg'
+import image6 from '@/public/images/place6.jpg'
+
+import image7 from '@/public/images/place7.jpg'
+import image8 from '@/public/images/place8.jpg'
+
+export const placesSamaraArray = [
+    {
+        title: 'ул.Вилоновская, 138"Е"',
+        time: "10.00 до 20.00, без выходных",
+        tel: "+7(846)922-93-87 (сот.)",
+        image: image1 ,
+    },
+    {
+        title: "пл. им Кирова, павильон 21",
+        time: "10.00 до 20.00, без выходных",
+        tel: "+7(937)202-04-51",
+        image: image2,
+    },
+    {
+        title: "Московское шоссе, д.205/1",
+        time: "12:00 до 20:00, Выходной: Понедельник",
+        tel: "+7(937)066-94-00",
+        image: image3,
+    },
+    {
+        title: "Московское шоссе 18-й км, д.25А",
+        time: "12:00 до 20:00 Выходной: Вторник",
+        tel: "+7(927)607-21-15",
+        image: image4,
+    },
+    {
+        title: 'Красноглинское шоссе, 3"Б"',
+        time: "10.00 до 20.00, без выходных",
+        tel: "+7(927)712-93-89",
+        image: image5,
+    },
+    {
+        title: "ул.Тамбовская, 2 (116 км)",
+        time: "Временно не работает",
+        tel: "+7(846)989-31-74 (сот.)",
+        image: image6,
+    },
+];
+
+export const placesNovokuybyshevskArray = [
+    {
+        title: "проспект Победы, д.36",
+        time: "10.00 до 22.00, без выходных",
+        tel: "+7(937)062-70-11",
+        image: image7,
+    },
+    {
+        title: 'проспект Победы, д. 1"Ж"',
+        time: "10.00 до 22.00, без выходных",
+        tel: "+7(995)933-63-63",
+        image: image8,
+    },
+];
