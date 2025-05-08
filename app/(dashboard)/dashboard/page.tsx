c5a1d0fdb9c71773d603437692926d815bacadd7
Anton <2022-02392@students.ssau.ru>
2025-05-08 17:44:44 +0400

    feat: создание компонента адмминки и реализация переадрисации в случае отсутствия сессии

---
+++ app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,18 @@
+
+import { getUserSession } from "@/lib/getUserSession";
+import { prisma } from "@/lib/prisma";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+    const session = await getUserSession();
+    if (!session) {
+        return redirect("/admin-login");
+    }
+    const user = await prisma.user.findFirst({ where: { id: session?.id } });
+
+  if (!user) {
+    return redirect('/not-auth');
+  }
+
+  return<>еее</>;
+}
