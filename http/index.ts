96411c5c54013bacf698f1008cd6a27b7b28028c
Anton <cart0003@mail.ru>
2025-05-15 19:50:13 +0400

    feat: изменения апи

--- http/index.ts
+++ http/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 // export const API_URL = 'https://68038e8e0a99cb7408ec5a5b.mockapi.io/'
-export const API_URL = 'http://localhost:3000/api'
+export const API_URL = 'https://aeaea.vercel.app/api'
 
 const $api = axios.create({
 	// withCredentials: true,
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath:'/AEAEA',
+  basePath:'/aeaea.vercel.app',
   images:{
     unoptimized:true
   }
